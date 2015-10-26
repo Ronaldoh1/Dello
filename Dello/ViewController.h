7f1fd04de1c403875d3d93dc16ea7df99f9034ce
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-25 18:55:30 -0700

    Initial Commit

---
+++ Dello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dello
+//
+//  Created by Ronald Hernandez on 10/25/15.
+//  Copyright © 2015 Dello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dello/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dello
+//
+//  Created by Ronald Hernandez on 10/25/15.
+//  Copyright © 2015 Dello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
