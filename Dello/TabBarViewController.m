b97a5704e1b1fb45571b3686ef81a6df4c5b2602
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-11-10 09:53:47 -0800

    refactored tabs

--- Dello/TabBarViewController.m
+++ Dello/TabBarViewController.m
@@ -38,7 +38,6 @@
 
     //Create the first Tab Bar, then use the storyboard to present the firstViewController
     UIStoryboard *listStoryBoard = [UIStoryboard storyboardWithName:@"List" bundle:nil];
-
     UITabBarController *listTabBar = [listStoryBoard instantiateViewControllerWithIdentifier:@"ListNavController"];
     [self addChildViewController:listTabBar];
     listTabBar.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"listTab.png"] scaledToSize:CGSizeMake(35, 35)];
@@ -47,7 +46,6 @@
 
     //Second Tab Bar
     UIStoryboard *groupStoryBoard = [UIStoryboard storyboardWithName:@"Group" bundle:nil];
-
     UITabBarController *groupTabBar = [groupStoryBoard instantiateViewControllerWithIdentifier:@"GroupNavController"];
     [self addChildViewController:groupTabBar];
     groupTabBar.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"groupTab.png"] scaledToSize:CGSizeMake(35, 35)];
@@ -56,18 +54,15 @@
 
 
     //Third Tab Bar
-    UIStoryboard *customListstoryBoard = [UIStoryboard storyboardWithName:@"Vote" bundle:nil];
-
-    UITabBarController *customTabBar = [customListstoryBoard instantiateViewControllerWithIdentifier:@"VoteNavController"];
-    [self addChildViewController:customTabBar];
-    customTabBar.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"voteTab.png"] scaledToSize:CGSizeMake(35, 35)];
-    customTabBar.tabBarItem.title = @"Vote";
-    customTabBar.tabBarItem.enabled = NO;
+    UIStoryboard *voteStoryboard = [UIStoryboard storyboardWithName:@"Vote" bundle:nil];
+    UITabBarController *voteTabBar = [voteStoryboard instantiateViewControllerWithIdentifier:@"VoteNavController"];
+    [self addChildViewController:voteTabBar];
+    voteTabBar.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"voteTab.png"] scaledToSize:CGSizeMake(35, 35)];
+    voteTabBar.tabBarItem.title = @"Vote";
+    voteTabBar.tabBarItem.enabled = NO;
 
     //Fourth Tab Bar
     UIStoryboard *resultsStoryBoard = [UIStoryboard storyboardWithName:@"Result" bundle:nil];
-
-
     UITabBarController *resultsTabBar = [resultsStoryBoard instantiateViewControllerWithIdentifier:@"ResultNavController"];
     [self addChildViewController:resultsTabBar];
     resultsTabBar.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"resulsTab.png"] scaledToSize:CGSizeMake(35, 35)];
@@ -76,7 +71,6 @@
 
     //Fifth Tab Bar
     UIStoryboard *discoverStoryBoard = [UIStoryboard storyboardWithName:@"Discover" bundle:nil];
-
     UITabBarController *discoverTabBar = [discoverStoryBoard instantiateViewControllerWithIdentifier:@"DiscoverNavController"];
     [self addChildViewController:discoverTabBar];
     discoverTabBar.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"discoverTab.png"] scaledToSize:CGSizeMake(35, 35)];
